085332039678e2c51893c25df7169a3fbf057b5f
Alex <Thier32@gmx.de>
2021-10-19 23:51:45 +0200

    main ts anpassungen

--- Script/Source/Main.ts
+++ Script/Source/Main.ts
@@ -10,9 +10,16 @@ namespace Script {
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    viewport.camera.mtxPivot.translateZ(-20); //ändert entfernung der Camera beim start des Spiels, ist hinzugefügt
   }
 
   function update(_event: Event): void {
+    
+    let speedAgentTranslation: number= 10; //meters per second
+    let speedAgentRotation: number = 360; //meters per second
+    let speedLaserRotate: number = 360; //degrees per second, bestimmt die game geschwindigkeit oder eher gesagt die rotationsgeschwindigkeit
+    TransformStream.rotateZ(speedLaserRotate * ƒ.Loop.timeFrameReal / 1000); //dazugehörige funktion
+
     // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
