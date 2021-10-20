06b583758cab70949523f11bca1da86f2587e6f9
Xanderthier <Thier32@gmx.de>
2021-10-21 00:15:57 +0200

    Movement update

--- Script/Build/Script.js
+++ Script/Build/Script.js
@@ -1,1 +1,85 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    let CustomComponentScript = /** @class */ (() => {
+        class CustomComponentScript extends ƒ.ComponentScript {
+            constructor() {
+                super();
+                // Properties may be mutated by users in the editor via the automatically created user interface
+                this.message = "CustomComponentScript added to ";
+                // Activate the functions of this component as response to events
+                this.hndEvent = (_event) => {
+                    switch (_event.type) {
+                        case "componentAdd" /* COMPONENT_ADD */:
+                            ƒ.Debug.log(this.message, this.node);
+                            break;
+                        case "componentRemove" /* COMPONENT_REMOVE */:
+                            this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                            this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                            break;
+                    }
+                };
+                // Don't start when running in editor
+                if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                    return;
+                // Listen to this component being added to or removed from a node
+                this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+            }
+        }
+        // Register the script as component for use in the editor via drag&drop
+        CustomComponentScript.iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        return CustomComponentScript;
+    })();
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    let transform;
+    let agent;
+    function start(_event) {
+        viewport = _event.detail;
+        let graph = viewport.getBranch();
+        console.log("graph" + graph);
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 60); //60 Bilder pro sekunde, frachtet auf framerate time rum anstatt realtime ,start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        viewport.camera.mtxPivot.translateZ(-20); //ändert entfernung der Camera beim start des Spiels, ist hinzugefügt
+        let laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser2")[0];
+        agent = graph.getChildrenByName("Agents")[0].getChildrenByName("Agent1")[0];
+        transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+    }
+    function update(_event) {
+        movement(_event);
+        //let speedAgentTranslation: number= 10; //meters per second
+        //let speedAgentRotation: number = 360; //meters per second
+        let speedLaserRotate = 360; //degrees per second, bestimmt die game geschwindigkeit oder eher gesagt die rotationsgeschwindigkeit
+        transform.rotateZ(speedLaserRotate * ƒ.Loop.timeFrameReal / 1000); //dazugehörige funktion
+        // ƒ.Physics.world.simulate();  // if physics is included and used
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+    function movement(_event) {
+        let deltaTime = ƒ.Loop.timeFrameReal / 1000;
+        let speedAgentTranslation = 10; // meters per second
+        let speedAgentRotation = 360; // meters per second
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP])) {
+            agent.mtxLocal.translateY(speedAgentTranslation * deltaTime);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])) {
+            agent.mtxLocal.translateY(-speedAgentTranslation * deltaTime);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+            agent.mtxLocal.rotateZ(-speedAgentRotation * deltaTime);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+            agent.mtxLocal.rotateZ(speedAgentRotation * deltaTime);
+        }
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Script/Source/Main.ts
+++ Script/Source/Main.ts
@@ -5,23 +5,61 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  let transform: ƒ.Matrix4x4;
+  let agent: ƒ.Node;
+
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    let graph: ƒ.Node = viewport.getBranch();
+    console.log("graph" + graph);
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update); 
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 60);  //60 Bilder pro sekunde, frachtet auf framerate time rum anstatt realtime ,start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     viewport.camera.mtxPivot.translateZ(-20); //ändert entfernung der Camera beim start des Spiels, ist hinzugefügt
+
+    let laser: ƒ.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser2")[0];
+
+    agent = graph.getChildrenByName("Agents")[0].getChildrenByName("Agent1")[0];
+
+    transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+
   }
 
   function update(_event: Event): void {
     
-    let speedAgentTranslation: number= 10; //meters per second
-    let speedAgentRotation: number = 360; //meters per second
+    movement(_event);
+
+    //let speedAgentTranslation: number= 10; //meters per second
+    //let speedAgentRotation: number = 360; //meters per second
     let speedLaserRotate: number = 360; //degrees per second, bestimmt die game geschwindigkeit oder eher gesagt die rotationsgeschwindigkeit
-    TransformStream.rotateZ(speedLaserRotate * ƒ.Loop.timeFrameReal / 1000); //dazugehörige funktion
+    transform.rotateZ(speedLaserRotate * ƒ.Loop.timeFrameReal / 1000); //dazugehörige funktion
 
     // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+  }
+
+  function movement(_event: Event): void {
+
+    let deltaTime: number = ƒ.Loop.timeFrameReal / 1000
+
+    let speedAgentTranslation: number = 10; // meters per second
+    let speedAgentRotation: number = 360; // meters per second
+
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP])) {
+      agent.mtxLocal.translateY(speedAgentTranslation * deltaTime)
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])) {
+      agent.mtxLocal.translateY(-speedAgentTranslation * deltaTime)
+    }
+
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+      agent.mtxLocal.rotateZ(-speedAgentRotation * deltaTime)
+    }
+
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+      agent.mtxLocal.rotateZ(speedAgentRotation * deltaTime)
+    }
   }
 }
