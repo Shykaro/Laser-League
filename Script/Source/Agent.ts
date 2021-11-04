f997831731d34a50041a053e5d3f9a90f5a9c809
Xanderthier <Thier32@gmx.de>
2021-11-04 14:22:16 +0100

    ?

--- Script/Source/Agent.ts
+++ Script/Source/Agent.ts
@@ -8,7 +8,8 @@ namespace Script {
 
 
             this.addComponent(new ƒ.ComponentMesh(new ƒ.MeshQuad("MeshAgent")));
-            this.addComponent(new ƒ.ComponentMaterial(new ƒ.Material("mtrAgent", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(1, 0, 1, 1)))));
+            this.addComponent(new ƒ.ComponentMaterial(
+                new ƒ.Material("mtrAgent", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(1, 0, 1, 1)))));
 
             this.mtxLocal.scale(ƒ.Vector3.ONE(0.5));
 
